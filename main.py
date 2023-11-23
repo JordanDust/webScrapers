9c926c2e0e765aa4cc679ff2a9f6764109279125
JasenDX <70975956+JasenDX@users.noreply.github.com>
2023-11-23 16:23:50 -0500

    Update main.py
    modified saveTo functions to work with new getVars function in webScraper.py

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 from datetime import datetime, time
-from webScraper import organizeVars
+from webScraper import getVars
 
 def saveToDisk(webType):
 	if(webType == "ecommerce"):
 		with open("eTextData.txt", "w") as file:
-            file.write(f"{date.today()} - {date.now()}: {\n organizeVars("ecommerce")}")
-
+            file.write(f"{date.today()} - {date.now()}: {\n getVars("ecommerce", "disk")}")
+												
 	elif(webType == "stocks"):
 		with open("sTextData.txt", "w") as file:
-            file.write(f"{date.today()} - {date.now()}: {\n organizeVars("stocks")}")
+            file.write(f"{date.today()} - {date.now()}: {\n organizeVars("stocks", "disk")}")
 
 	else:
 		print("ERROR - printing to Disk")
 
 def writeToGui(webType):
 	if((webType == "ecommerce") or (webType == "stocks)):
-        return organizeVars(webType)
+        return getVars(webType, "gui")
     else:
         print("ERROR - could not write to GUI")
