0f88b833a9bd44e953dfa31a77211ccd6e759742
JasenDX <70975956+JasenDX@users.noreply.github.com>
2023-11-23 15:59:34 -0500

    Update main.py
    modified some webScraper.py programs to work in main

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-from datetime import time
+from datetime import datetime, time
+from webScraper import organizeVars
+
+def saveToDisk(webType):
+	if(webType == "ecommerce"):
+		with open("eTextData.txt", "w") as file:
+            file.write(f"{date.today()} - {date.now()}: {\n organizeVars("ecommerce")}")
+
+	elif(webType == "stocks"):
+		with open("sTextData.txt", "w") as file:
+            file.write(f"{date.today()} - {date.now()}: {\n organizeVars("stocks")}")
+
+	else:
+		print("ERROR - printing to Disk")
+
+def writeToGui(webType):
+	if((webType == "ecommerce") or (webType == "stocks)):
+        return organizeVars(webType)
+    else:
+        print("ERROR - could not write to GUI")
